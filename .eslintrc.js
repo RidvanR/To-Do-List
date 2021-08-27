f760679013131da0271bb822936c93eab6748ea5
Jonathan Graf <jonathangraf57@gmail.com>
2021-08-27 10:03:08 +0200

    fix exlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
-  overrides: [
-	{
-		files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-		'linebreak-style': 'error',
-		'no-tabs': ['error', { allowIndentationTabs: true }],
-	}
-  ],
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+	overrides: [
+		{
+			files: ['*.js'],
+			rules: {
+				'linebreak-style': 'off',
+				'no-tabs': ['error', { allowIndentationTabs: true }],
+				indent: ['error', 'tab'],
+			},
+		},
+		{
+			files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+			rules: {
+				'react/jsx-indent': ['error', 'tab', { checkAttributes: true, indentLogicalExpressions: true }],
+				'react/jsx-indent-props': ['error', 'tab'],
+				'linebreak-style': 'off',
+				'no-tabs': ['error', { allowIndentationTabs: true }],
+				indent: ['error', 'tab'],
+			},
+		},
+	],
 };
